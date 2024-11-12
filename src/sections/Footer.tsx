0cfef0ab73e79aca690f5d882abd95b0270ac215
netrobase <netrobase@gmail.com>
2024-11-13 00:44:49 +0100

    Update Footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -5,7 +5,7 @@ import Youtube from "@/assets/social-youtube.svg"
 import Link from "next/link";
 export const Footer = () => {
   return(
-    <footer className="py-5 border-t border-white/15">
+    <footer className="py-5 px-5 border-t border-white/15">
       <div className="container">
         <div className="flex flex-col lg:flex-row lg:items-center gap-8"> 
         <div className="flex gap-2 items-center lg:flex-1">
