474fdfd36de3a64264c146dd347cdd4d8e5d0030
netrobase <netrobase@gmail.com>
2024-11-14 22:27:14 +0100

    Update Layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 import { Hero } from "@/sections/Hero";
 import { About } from "@/sections/About";
-import { Gallery } from "@/sections/Gallery";
+import { Gallery } from "@/sections/Gallery"
 
 export default function Home() {
   return (
